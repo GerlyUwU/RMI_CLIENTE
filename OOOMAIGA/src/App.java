9eb2859b287887dea385548681d5b69c300c462c
GerlyUwU <arteaga.bernal4@gmail.com>
2024-04-18 06:39:11 -0600

    actualizamos las clases

--- OOOMAIGA/src/App.java
+++ OOOMAIGA/src/App.java
@@ -1,5 +1,34 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import Interface.IPersonaController;
+import Interface.IPersona;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        try {
+            IPersonaController personaController = (IPersonaController) Naming
+                    .lookup("rmi://localhost/personaController");
+
+           List<IPersona> lista =  personaController.list();
+           for( IPersona persona : lista){
+            System.out.println(persona.getString());
+           }
+
+
+        } catch (NotBoundException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (RemoteException ex) {
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
+
 }
---
+++ OOOMAIGA/src/Interface/IPersonaController.java
@@ -0,0 +1,20 @@
+package Interface;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.List;
+
+public interface IPersonaController extends Remote {
+
+    IPersona newInstance() throws RemoteException;
+    int add(IPersona persona) throws RemoteException;
+    List<IPersona> list() throws RemoteException;
+    void update() throws RemoteException;
+
+    int ADD_EXITO        = 1; 
+    int ADD_ID_DUPLICADO = 2;
+    int ADD_SIN_EXITO    = 3; 
+ 
+    int UPADATE_EXITO    = 1;
+    int UPDATE_INEXISTE  = 2;
+}
